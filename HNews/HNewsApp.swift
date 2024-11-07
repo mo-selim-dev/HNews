eeae63ad06991b8d2eeca1579f7c9772cafaafaf
Mohammed Selim <mo.selim.dev@gmail.com>
2024-11-07 02:08:25 +0200

    Initial Commit

---
+++ HNews/HNewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  HNewsApp.swift
+//  HNews
+//
+//  Created by Mohamed Selim on 07/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct HNewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ HNewsTests/HNewsTests.swift
@@ -0,0 +1,17 @@
+//
+//  HNewsTests.swift
+//  HNewsTests
+//
+//  Created by Mohamed Selim on 07/11/2024.
+//
+
+import Testing
+@testable import HNews
+
+struct HNewsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
