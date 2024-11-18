b6cc1012a291eaf385a5290c7fa35f63ae04e91b
Mohammed Selim Dev <mo.selim.dev@gmail.com>
2024-11-19 00:44:42 +0200

    Update ContentView.swift

--- HNews/ContentView.swift
+++ HNews/ContentView.swift
@@ -2,23 +2,63 @@
 //  ContentView.swift
 //  HNews
 //
-//  Created by Mohamed Selim on 07/11/2024.
-//
+//  Created by Mohamed Selim on 04/11/2024.
 
 import SwiftUI
 
 struct ContentView: View {
+
+    @ObservedObject var networkManager = NetworkManager()
+
+    init() {
+               UINavigationBar.appearance().barTintColor = UIColor.green 
+               UINavigationBar.appearance().tintColor = UIColor.white 
+        UINavigationBar.appearance().titleTextAttributes = [NSAttributedString.Key.font: UIFont.boldSystemFont(ofSize: 25),
+                                                            NSAttributedString.Key.foregroundColor: UIColor.systemBlue]
+
+    }
+
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+            List(networkManager.posts) { post in
+                NavigationLink(destination: DetailView(url: post.url)) {
+                    HStack {
+                        Text(String(post.points))
+                            .font(.headline)
+                            .foregroundColor(.blue).opacity(0.8)
+
+                            .frame(width: 50, alignment: .leading)
+
+                        VStack(alignment: .leading) {
+                            Text(post.title)
+                                .font(.subheadline)
+                                .fontWeight(.bold)
+                                .foregroundColor(.black).opacity(0.8)
+
+                            if let url = post.url {
+                                Text(url)
+                                    .font(.footnote)
+                                    .foregroundColor(.gray)
+
+                            }
+                        }
+                    }
+                    .padding(.vertical, 5) 
+                }
+            }
+            .navigationTitle("Hacker News")
+            .navigationBarTitleDisplayMode(.inline)
+
+
         }
-        .padding()
+        .onAppear {
+            networkManager.fetchData()
+        }
     }
 }
+
 
 #Preview {
     ContentView()
 }
+
